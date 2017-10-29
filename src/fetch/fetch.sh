983d2e39cbf0bb9d08a783c23dc7a157d377e16d
bahorn <horn@uni.coventry.ac.uk>
2017-10-29 07:12:32 +0000

    imporved fetch.sh

--- src/fetch/fetch.sh
+++ src/fetch/fetch.sh
@@ -1,6 +1,6 @@
-NORMAL_TARGET=blah.nyc3.digitaloceanspaces.com
-LEGIT_TARGET=bahorn.nyc3.digitaloceanspaces.com
+NORMAL_TARGET=blah.nyc3.digitaloceanspaces.com # This is what other people see
+LEGIT_TARGET=bahorn.nyc3.digitaloceanspaces.com # only the host sees this.
 UUID=$1
 DATA=`curl --header "Host: $LEGIT_TARGET" https://$NORMAL_TARGET/dat/$UUID.blob 2>/dev/null`
 # combine with tor here.
-echo Bridge $DATA
+echo Bridge $DATA | cat torrc - >latest_torrc
